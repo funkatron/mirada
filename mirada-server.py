c54f44b8260175daa11892c39632261c688d6b6a
Edward Finkler <efinkler@delmarsd.com>
2021-02-22 00:04:55 -0500

    Implement project freezing with `mirada-server freeze` - update bootstrap vue example - use local version of polyfill - update Jinja2 vairavle start string to be `{{=`, which should avoid conflicts with Vue templates - add _dist/* to .gitignore

--- mirada-server.py
+++ mirada-server.py
@@ -3,11 +3,15 @@ import os
 import shutil
 import subprocess
 import sys
+from typing import List
 
 import click
 from dotenv import load_dotenv
 
 from flask.cli import pass_script_info
+from flask_frozen import Freezer
+
+from mirada import create_mirada_app
 
 IS_BUNDLED = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
 # register the mirada admin blueprint
@@ -69,13 +73,51 @@ def init(script_info, project_path: str = None):
 @cli.command()
 @click.option('--project_path', default=os.getcwd())
 @click.option('--output_path', default=os.path.join(os.getcwd(), '_dist'))
-@pass_script_info
-def freeze(script_info, project_path: str = None):
+def freeze(project_path: str = None, output_path: str = None):
     """Freezes the project to the output path, generating a static HTML site."""
     if project_path is None:
         project_path = os.getcwd()
-    raise NotImplementedError
-    # create_mirada_app(project_path)
+    if output_path is None:
+        output_path = os.path.join(project_path, '_dist')
+
+    click.echo(f'Freezing site to {os.path.abspath(output_path)}...')
+
+    app = create_mirada_app(project_path, include_debug_toolbar=False, include_mirada_admin_bp=False)
+    app.config['FREEZER_DESTINATION'] = output_path
+    app.config['DEBUG'] = False
+    freezer = Freezer(
+        app,
+        with_no_argument_rules=False,
+        log_url_for=False,
+    )
+
+    @freezer.register_generator
+    def page_uri_path_generator():
+        pages_folder = os.path.join(app.template_folder, 'pages')
+        for template_path in get_page_template_paths(pages_folder):
+            yield 'mirada.blueprints.pages_blueprint.render_page', dict(path_str=f'{template_path.replace(pages_folder, "")}')
+
+    with click.progressbar(freezer.freeze_yield(), item_show_func=lambda p: p.url if p else 'Done!') as urls:
+        for url in urls:
+            # everything is already happening, just pass
+            pass
+
+    click.echo(f'Finished generating site in {os.path.abspath(app.config["FREEZER_DESTINATION"])}')
+
+
+def get_page_template_paths(template_root_path: str = None, template_paths: List[str] = None) -> List[str]:
+    if template_paths is None:
+        template_paths = []
+    for root, folders, files in os.walk(template_root_path):
+        template_paths.extend([
+            os.path.join(root, filename)
+            for filename in files
+        ])
+        for folder in folders:
+            template_paths.extend(
+                get_page_template_paths(template_root_path=os.path.join(root, folder), template_paths=template_paths)
+            )
+    return template_paths
 
 
 if __name__ == "__main__":
--- mirada/__init__.py
+++ mirada/__init__.py
@@ -15,9 +15,21 @@ if IS_BUNDLED:
     MIRADA_PROJ_PATH = os.path.abspath(getattr(sys, '_MEIPASS'))
 
 
+class MiradaFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='{%',
+        block_end_string='%}',
+        variable_start_string='{{=',
+        variable_end_string='}}',
+        comment_start_string='{#',
+        comment_end_string='#}',
+    ))
+
+
 def create_app(name=__name__, template_folder: str = None, static_folder: str = None, debug: bool = None):
     # get the path to the directory where this file is located
-    app = Flask(
+    app = MiradaFlask(
         name,
         template_folder=template_folder,
         static_folder=static_folder,
@@ -29,7 +41,7 @@ def create_app(name=__name__, template_folder: str = None, static_folder: str = 
     return app
 
 
-def create_mirada_app(project_path: str = None):
+def create_mirada_app(project_path: str = None, include_debug_toolbar=True, include_mirada_admin_bp=True):
     if project_path is None:
         project_path = os.path.expanduser(os.getenv('MIRADA_CURRENT_PROJECT_PATH', default=os.getcwd()))
 
@@ -40,11 +52,14 @@ def create_mirada_app(project_path: str = None):
     project_static_folder = os.path.abspath(os.path.join(project_path, 'static'))
     mirada_app = create_app(name='mirada', template_folder=project_template_folder, static_folder=project_static_folder)
 
-    from flask_debugtoolbar import DebugToolbarExtension
-    DebugToolbarExtension(mirada_app)
+    if include_debug_toolbar is True:
+        from flask_debugtoolbar import DebugToolbarExtension
+        DebugToolbarExtension(mirada_app)
 
     mirada_app.logger.info(f'Mirada Server Startup')
     mirada_app.logger.info(f'project_path="{project_path}"')
+
+    mirada_app.config['TEMPLATES_AUTO_RELOAD'] = True
 
     mirada_app.register_blueprint(
         pages_bp,
@@ -55,13 +70,13 @@ def create_mirada_app(project_path: str = None):
     mirada_app.logger.info(f'template_folder="{project_template_folder}"')
     mirada_app.logger.info(f'static_folder="{project_static_folder}"')
 
-    mirada_template_folder = os.path.join(MIRADA_PROJ_PATH, 'templates')
-    mirada_static_folder = os.path.join(MIRADA_PROJ_PATH, 'static')
-    mirada_app.register_blueprint(admin_bp, url_prefix='/_mirada', template_folder=mirada_template_folder, static_folder=mirada_static_folder)
-
-    mirada_app.logger.debug(f'admin mounted to /_mirada')
-    mirada_app.logger.debug(f'template_folder="{mirada_template_folder}"')
-    mirada_app.logger.debug(f'static_folder="{mirada_static_folder}"')
+    if include_mirada_admin_bp is True:
+        mirada_template_folder = os.path.join(MIRADA_PROJ_PATH, 'templates')
+        mirada_static_folder = os.path.join(MIRADA_PROJ_PATH, 'static')
+        mirada_app.register_blueprint(admin_bp, url_prefix='/_mirada', template_folder=mirada_template_folder, static_folder=mirada_static_folder)
+        mirada_app.logger.debug(f'admin mounted to /_mirada')
+        mirada_app.logger.debug(f'template_folder="{mirada_template_folder}"')
+        mirada_app.logger.debug(f'static_folder="{mirada_static_folder}"')
 
     return mirada_app
 
